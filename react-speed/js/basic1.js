e09b6f343f8329a6bbd68fd63ad713a9e3eab782
subin <subin9033@gmail.com>
2020-12-29 08:26:00 +0900

    Fix : speed game js 수정2

--- react-speed/js/basic1.js
+++ react-speed/js/basic1.js
@@ -4,7 +4,6 @@ let startTime, endTime;
 let records = [];
 let timeoutId;
 
-let forjs = undefined;
 $screen.addEventListener("click", function () {
   if ($screen.classList.contains("waiting")) {
     //대기화면
