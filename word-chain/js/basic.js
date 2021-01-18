3a1a464f9b1fef1a8fab6e217919ccb156d4b080
subin <subin9033@gmail.com>
2021-01-19 07:25:00 +0900

    Fix : w-chain game js

--- word-chain/js/basic.js
+++ word-chain/js/basic.js
@@ -33,9 +33,3 @@ const onInput = (e) => {
 
 $button.addEventListener("click", onClickBtn); // onClickBtn() 괄호를 붙이게 되면 이벤트와 상관없이 함수가 실행됨.
 $input.addEventListener("input", onInput);
-
-//1.프로그램 절차의 개수는 정해져 있어야 한다.
-//2.각 절차는 항상 같은 내용이어야 한다.
-//3.모든 가능성을 고려해야 한다.
-//4.예시는 절차를 검증하기 위해 사용한다.
-//* 사용자의 이벤트 (버튼클릭, 입력창 글자 입력 등)가 필요한 곳에서 순서도를 끊어야 함.
